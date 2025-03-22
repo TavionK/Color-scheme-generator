3faf0e1da0b53c3cdb0314ee8d88046e999db426
TavionK <tavion.k.britt99@gmail.com>
2025-03-21 21:47:26 -0400

    made button fetch the color api with the inputted color and mode

--- index.js
+++ index.js
@@ -2,7 +2,12 @@ const baseUrl = 'https://www.thecolorapi.com/scheme'
 const getColorsBtn = document.getElementById('get-colors-btn')
 const colorModeSelector = document.getElementById('color-mode-selector')
 const colorPicker = document.getElementById('color-picker')
+
 getColorsBtn.addEventListener('click',function(e){
-    console.log(colorModeSelector.value)
-    console.log(colorPicker.value)
+    const hex = colorPicker.value.replace("#", "")
+    const url = baseUrl + `?hex=${hex}&mode=${colorModeSelector.value}`
+    console.log(url)
+    fetch(url)
+        .then(res => res.json())
+        .then(data => console.log(data))
 })

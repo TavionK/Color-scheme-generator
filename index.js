02ff2ce2336d20a69ff3aad5ef97b80ddf447488
TavionK <tavion.k.britt99@gmail.com>
2025-03-21 21:36:03 -0400

    added js variables to get the value of the color mode selector and color picker

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const baseUrl = 'https://www.thecolorapi.com/scheme'
 const getColorsBtn = document.getElementById('get-colors-btn')
+const colorModeSelector = document.getElementById('color-mode-selector')
+const colorPicker = document.getElementById('color-picker')
 getColorsBtn.addEventListener('click',function(e){
-    console.log("button works")
+    console.log(colorModeSelector.value)
+    console.log(colorPicker.value)
 })

4b370110b8b71fb6a7ef8f1f268d3ed1e8d839af
TavionK <tavion.k.britt99@gmail.com>
2025-03-21 22:45:39 -0400

    display 5 colors to screen

--- index.js
+++ index.js
@@ -3,16 +3,29 @@ const getColorsBtn = document.getElementById('get-colors-btn')
 const colorModeSelector = document.getElementById('color-mode-selector')
 const colorPicker = document.getElementById('color-picker')
 
+let colorsArr = []
 getColorsBtn.addEventListener('click',function(e){
-    let colorsArr = []
+    colorsArr = []
     const hex = colorPicker.value.replace("#", "")
     const url = baseUrl + `?hex=${hex}&mode=${colorModeSelector.value}`
-    console.log(url)
     fetch(url)
         .then(res => res.json())
         .then(data => {
             for (const c in data.colors){
                 colorsArr.push(data.colors[c].hex.value)
+                //console.log(typeof(data.colors[c].hex.value))
             }
+            renderColors()
         })
 })
+
+function getColorsHtml(){
+    const colorsHtml = colorsArr.map(function(color){
+        return `<p>${color}</p>`
+    })
+    return colorsHtml.join('')
+}
+
+function renderColors(){
+    document.getElementById("color-section").innerHTML = getColorsHtml()
+}

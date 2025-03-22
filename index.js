07d17b014bbb7edf098bdbf10905611538de76db
TavionK <tavion.k.britt99@gmail.com>
2025-03-21 21:25:31 -0400

    added event listener for the get color scheme button

---
+++ index.js
@@ -0,0 +1,5 @@
+const baseUrl = 'https://www.thecolorapi.com/scheme'
+const getColorsBtn = document.getElementById('get-colors-btn')
+getColorsBtn.addEventListener('click',function(e){
+    console.log("button works")
+})
